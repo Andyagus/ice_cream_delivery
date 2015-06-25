9f96ade0ff22b96056f069879eda475254ca35d9
Preston Moore <prestonmoore@me.com>
2015-06-25 16:50:11 -0400

    formatted nav bar and footer on layout.erb, with icon, alignment of nav list items

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+get '/home' do 
+  @title = 'Home'
+  erb :home
+end
+
+get '/order' do 
+  @title = 'Order Now'
+  erb :order
+end
+
+get '/runner' do 
+  @title = 'Become A Runner'
+  erb :runner
+end
+
+get '/contact' do 
+  @title = 'Contact Us'
+  erb :contact
+end
+
+post '/congrats' do 
+  @title = 'Congradulations!'
+  @username = params['username']
+  password = params['password']
+  erb :congrats
+end
