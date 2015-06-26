acda58f0bcf1acb20888efa884ff7440de92ad74
Andy Agus <Redagus@gmail.com>
2015-06-26 18:23:49 -0400

    redone order page and css elements

--- app.rb
+++ app.rb
@@ -23,8 +23,27 @@ get '/contact' do
 end
 
 post '/congrats' do 
-  @title = 'Congradulations!'
+  @title = 'Congratulations!'
   @username = params['username']
   password = params['password']
   erb :congrats
+end
+
+
+post '/order-success' do 
+  @title = 'Order Success!'
+  @username = params['name_existing']
+  @password = params['password_existing']
+   "Thanks for your order #{@username}"
+end
+
+post '/order-signup' do 
+  @title = 'Order Success!'
+  @guest_firstname = params['firstname_guest']
+  @guest_lastname = params['lastname_guest']
+  @guest_password = params['email_guest']
+  @guest_phone = params['phone_guest']
+  @guest_password = params['password_guest']
+  @guest_passwordconfirm = params['confirmpassword_guest']
+    "Thanks for signing up! #{@guest_firstname}"
 end
