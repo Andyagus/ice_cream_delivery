987dcee931d0e2cdb519ac72fdd5f3a40af29d0b
Andy Agus <Redagus@gmail.com>
2015-06-28 18:10:41 -0400

    Added funcationality to the cones

--- public/main.js
+++ public/main.js
@@ -17,19 +17,37 @@ $( document ).ready(function() {
 
 // ORDER (Andy)
 
-$('.item').hide(); 
-$('.divBox').hover(function() { 
-    $(this).children('.item').show();     
-}, function() { 
-    $(this).children('.item').hide(); 
+$(".mover").mousemove(function(event){
+      $(".wide_icecream").css('left','-520px');
+      $(".flavor p").text("French Vanilla")
+});
+
+$(".mover2").mousemove(function(event){
+      $(".wide_icecream").css('left','-1028px');
+      $(".flavor p").text("Triple Chocolate")
+});
+
+$(".mover3").mousemove(function(event){
+      $(".wide_icecream").css('left','-1545px');
+      $(".flavor p").text("Strawberry Slam")
+});
+
+$(".mover4").mousemove(function(event){
+      $(".wide_icecream").css('left','-2068px');
+      $(".flavor p").text("Pistachio Mustachio")
 });
 
 
+$(".mover5").mousemove(function(event){
+      $(".wide_icecream").css('left','-2568px');
+      $(".flavor p").text("Simply Vanilla")
 
+});
 
-
-
-
+$(".mover6").mousemove(function(event){
+      $(".wide_icecream").css('left','-3078px');
+      $(".flavor p").text("Princess Peach")
+});
 
 // BECOME A RUNNER (Jake)
 
